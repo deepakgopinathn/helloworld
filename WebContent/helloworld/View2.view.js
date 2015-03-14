a9512319075ea4011676889110e4da4f5d138f56
Deepak Gopinath <deepakgopinath.n@gmail.com>
2015-03-14 20:40:14 +0530

    CR-002 : Second Commit

--- WebContent/helloworld/View2.view.js
+++ WebContent/helloworld/View2.view.js
@@ -1,21 +1,22 @@
-sap.ui.jsview("helloworld.View1", {
+sap.ui.jsview("helloworld.View2", {
 
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf helloworld.View1
+	* @memberOf helloworld.View2
 	*/ 
 	getControllerName : function() {
-		return "helloworld.View1";
+		return "helloworld.View2";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf helloworld.View1
+	* @memberOf helloworld.View2
 	*/ 
 	createContent : function(oController) {
-			var oLabel = new sap.ui.commons.Label("l1");
-			oLabel.setText("Hello World Changes For Diff");
-			return oLabel;
+		var oLabel = new sap.ui.commons.Label("l1");
+		oLabel.setText("View 2 : Hello World Changes For Diff");
+		return oLabel;
+
 	}
 
 });
