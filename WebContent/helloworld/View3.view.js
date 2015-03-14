f5bce9a428dd35a47cb8e4bb55bf8f83bbd88db1
Deepak Gopinath <deepakgopinath.n@gmail.com>
2015-03-14 20:41:46 +0530

    CR-003 : Third Commit - View 3 created

--- WebContent/helloworld/View3.view.js
+++ WebContent/helloworld/View3.view.js
@@ -1,22 +1,21 @@
-sap.ui.jsview("helloworld.View2", {
+sap.ui.jsview("helloworld.View3", {
 
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf helloworld.View2
+	* @memberOf helloworld.View3
 	*/ 
 	getControllerName : function() {
-		return "helloworld.View2";
+		return "helloworld.View3";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf helloworld.View2
+	* @memberOf helloworld.View3
 	*/ 
 	createContent : function(oController) {
 		var oLabel = new sap.ui.commons.Label("l1");
-		oLabel.setText("View 2 Changed: Hello World Changes For Diff");
+		oLabel.setText("View 3 : Hello World Changes For Diff");
 		return oLabel;
-
 	}
 
 });
